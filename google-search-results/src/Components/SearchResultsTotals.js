880270304148ae2101bf34ae0bd45012d1fd7f90
jason-nordheim <jason.nordheim@gmail.com>
2020-06-05 13:54:33 -0600

    seperated the totals part of the search results

---
+++ google-search-results/src/Components/SearchResultsTotals.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SearchResultsTotals extends Component {
+    render() {
+        return (
+            <p class="totals">About 15500000 search results  (0.76 seconds)</p>
+        )
+    }
+}
